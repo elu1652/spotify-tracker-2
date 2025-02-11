5e1d66cf611663debe9e01af8a89f0bef9169778
elu1652 <166194412+elu1652@users.noreply.github.com>
2025-02-11 11:30:02 -0500

    update

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,14 +9,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDlndpYc8izUJjkL0Ol5uq75kO54b5_WmU",
-  authDomain: "spotify-tracker-b8e04.firebaseapp.com",
-  databaseURL: "https://spotify-tracker-b8e04-default-rtdb.firebaseio.com",
-  projectId: "spotify-tracker-b8e04",
-  storageBucket: "spotify-tracker-b8e04.firebasestorage.app",
-  messagingSenderId: "425664511144",
-  appId: "1:425664511144:web:074aa12e79d0d9720b969d",
-  measurementId: "G-MBCL489NDP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
